32fdc37a2f7b8005174b51911beed482b3bbfb5a
HMuraja <hilla.muraja@hotmail.com>
2023-09-19 14:11:03 +0000

    Add frontend

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 import os
 import dj_database_url
-import re
 
 if os.path.exists('env.py'):
     import env
@@ -59,7 +58,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
@@ -107,14 +106,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(
-        r'^.+-',
-        os.environ.get('CLIENT_ORIGIN_DEV', ''),
-        re.IGNORECASE
-        ).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+CORS_ALLOWED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN'),
     ]
 
 # if 'CLIENT_ORIGIN' in os.environ:
